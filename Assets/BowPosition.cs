1125b26d2ef757067c17937c80199f5ca0c6db71
Roey_Aharonov <roeyah@matrixdna.ai>
2023-09-01 16:08:20 +0300

    create bow position script

---
+++ Assets/BowPosition.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowPosition : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _radius;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = (mousePosition - (Vector2)_player.position).normalized;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        transform.position = (Vector2)_player.position + direction * _radius;
+        transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+}
